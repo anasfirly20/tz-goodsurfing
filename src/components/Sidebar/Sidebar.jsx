37ffacece38851af0a9be8748eed45fe5ed10549
Anas Firly <anasfirly20@gmail.com>
2023-09-12 20:50:29 +0400

    adding sidenav_items

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,9 @@
 // Miscellaneous
 import { Icon } from "@iconify/react";
 
+// Constants
+import { sidenav_items } from "../../constants/constants";
+
 export default function Sidebar() {
   return (
     <nav className="bg-white fixed left-0 h-full w-[320px] drop-shadow-[3px_3px_8px_#00000008] pt-normal">
@@ -14,6 +17,20 @@ export default function Sidebar() {
           className="text-custom-gray-2"
         />
       </button>
+      <section className="flex flex-col justify-center items-center mt-shorter2">
+        <section className="flex flex-col gap-5">
+          {sidenav_items.map((item) => (
+            <button
+              key={item.id}
+              className="flex items-center gap-5 text-custom-gray-2"
+            >
+              <Icon icon={item.icon} fontSize={40} />
+              <p className="">{item.label}</p>
+            </button>
+          ))}
+        </section>
+        <div className=""></div>
+      </section>
     </nav>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,32 @@
+export const sidenav_items = [
+  {
+    id: 1,
+    icon: "carbon:home",
+    label: "Рабочий стол",
+    subItems: ["Example 1", "Example 2", "Example 3", "Example 4"],
+  },
+  {
+    id: 2,
+    icon: "carbon:globe",
+    label: "Вакансии",
+    subItems: ["Example 1", "Example 2", "Example 3", "Example 4"],
+  },
+  {
+    id: 3,
+    icon: "tabler:mail-down",
+    label: "Заявки",
+    subItems: ["Example 1", "Example 2", "Example 3", "Example 4"],
+  },
+  {
+    id: 4,
+    icon: "ep:calendar",
+    label: "Календарь",
+    subItems: ["Example 1", "Example 2", "Example 3", "Example 4"],
+  },
+  {
+    id: 5,
+    icon: "octicon:organization-16",
+    label: "Организация",
+    subItems: ["Example 1", "Example 2", "Example 3", "Example 4"],
+  },
+];
